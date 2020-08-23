a3f828f9b569b2b36297bf51612217e1ab1d7e49
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-08-23 12:26:30 -0300

    create model Recipe

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.CharField(max_length=300)
+    preparation_mode = models.CharField(max_length=300)
+    time_mode = models.IntegerField(validators=[MinValueValidator(1)])
+    Yield = models.CharField(max_length=300)
+    category = models.CharField(max_length=100)
+    date_of_created = models.DateField(default=datetime.now())
