097d3a5cdbda8dbf30e2995c5f2f659caa0dc126
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-08-23 11:14:14 -0300

    for inof of template

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html')
+    data = {}
+    name_of_recipes = {
+        1:'bean',
+        2:'rice',
+        3:'meat'
+    }
+
+
+    return render(request,'home.html',{'name_of_recipes':name_of_recipes})
 
 def recipes(request):
     return render(request,'recipes.html')
