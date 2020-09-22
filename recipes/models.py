710e84f1b31da6a024c357b56d80594b73fe4083
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-09-22 18:22:43 -0300

    models

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,23 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
     ingredients = models.CharField(max_length=300)
     date_of_created = models.DateField(default=datetime.now(), blank=True)
+    time_preparation = models.TimeField()
 
+    WITHOUT = 'WI'
+    PASTA = 'PA'
+    VEGAN = 'VE'
+    CANDY = 'CA'
+    STATUS_CATEGORY = (
+        (PASTA, 'Pasta'),
+        (WITHOUT, 'Without'),
+        (VEGAN, 'Vegan'),
+        (CANDY, 'Candy'),
+    )
+    category = models.CharField(
+        max_length=2,
+        choices=STATUS_CATEGORY,
+        default=WITHOUT
+    )
 
 
 
